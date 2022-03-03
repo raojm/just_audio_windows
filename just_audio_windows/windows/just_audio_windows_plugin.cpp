fdc5f93bb75ee9bf19c350b680e715dc153e6269
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2022-03-03 19:57:46 -0300

    Implement main player methods
    play, pause, setVolume, setSpeed, setPitch and seek

--- just_audio_windows/windows/just_audio_windows_plugin.cpp
+++ just_audio_windows/windows/just_audio_windows_plugin.cpp
@@ -20,6 +20,21 @@ using flutter::EncodableValue;
 
 // Looks for |key| in |map|, returning the associated value if it is present, or
 // a nullptr if not.
+//
+// The variant types are mapped with Dart types in following ways:
+// std::monostate       -> null
+// bool                 -> bool
+// int32_t              -> int
+// int64_t              -> int
+// double               -> double
+// std::string          -> String
+// std::vector<uint8_t> -> Uint8List
+// std::vector<int32_t> -> Int32List
+// std::vector<int64_t> -> Int64List
+// std::vector<float>   -> Float32List
+// std::vector<double>  -> Float64List
+// EncodableList        -> List
+// EncodableMap         -> Map
 const EncodableValue* ValueOrNull(const EncodableMap& map, const char* key) {
   auto it = map.find(EncodableValue(key));
   if (it == map.end()) {
--- just_audio_windows/windows/player.hpp
+++ just_audio_windows/windows/player.hpp
@@ -39,9 +39,75 @@ public:
     void AudioPlayer::HandleMethodCall(
         const flutter::MethodCall<flutter::EncodableValue> &method_call,
         std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-            const auto* args =std::get_if<flutter::EncodableMap>(method_call.arguments());
+        const auto* args =std::get_if<flutter::EncodableMap>(method_call.arguments());
         if (args) {
-            if (method_call.method_name().compare("init") == 0) {
+            if (method_call.method_name().compare("load") == 0) {
+            } else if (method_call.method_name().compare("play") == 0) {
+              mediaPlayer.Play();
+              result->Success();
+            } else if (method_call.method_name().compare("pause") == 0) {
+              mediaPlayer.Pause();
+              result->Success();
+            } else if (method_call.method_name().compare("setVolume") == 0) {
+              const auto* volume = std::get_if<double>(ValueOrNull(*args, "volume"));
+              if (!volume) {
+                return result->Error("volume_error", "volume argument missing");
+              }
+              float volumeFloat = (float) *volume;
+              mediaPlayer.Volume(volumeFloat);
+              result->Success();
+            } else if (method_call.method_name().compare("setSpeed") == 0) {
+              const auto* speed = std::get_if<double>(ValueOrNull(*args, "speed"));
+              if (!speed) {
+                return result->Error("speed_error", "speed argument missing");
+              }
+              float speedFloat = (float) *speed;
+              mediaPlayer.PlaybackRate(speedFloat);
+              result->Success();
+            } else if (method_call.method_name().compare("setPitch") == 0) {
+              const auto* pitch = std::get_if<double>(ValueOrNull(*args, "pitch"));
+              if (!pitch) {
+                return result->Error("pitch_error", "pitch argument missing");
+              }
+              float pitchFloat = (float) *pitch;
+              mediaPlayer.AudioBalance(pitchFloat);
+              result->Success();
+            } else if (method_call.method_name().compare("setSkipSilence") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("setLoopMode") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("setShuffleMode") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("setShuffleOrder") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("setAutomaticallyWaitsToMinimizeStalling") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("setCanUseNetworkResourcesForLiveStreamingWhilePaused") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("setPreferredPeakBitRate") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("seek") == 0) {
+              const auto* position = std::get_if<int32_t>(ValueOrNull(*args, "position"));
+              if (position) {
+                mediaPlayer.Position(TimeSpan(std::chrono::milliseconds(*position)));
+              }
+              result->Success();
+            } else if (method_call.method_name().compare("concatenatingInsertAll") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("concatenatingRemoveRange") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("concatenatingMove") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("setAndroidAudioAttributes") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("audioEffectSetEnabled") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("androidLoudnessEnhancerSetTargetGain") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("androidEqualizerGetParameters") == 0) {
+              result->Success();
+            } else if (method_call.method_name().compare("androidEqualizerBandSetGain") == 0) {
+              result->Success();
             } else {
               result->NotImplemented();
             }
